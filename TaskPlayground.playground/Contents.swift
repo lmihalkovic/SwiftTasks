fd3a08c7e8c7b810efcb1223decdedba2cdd8bdb
L Mihalkovic <laurentm@codingblues.io>
2019-06-13 17:01:03 +0200

    switched to 10.xx

--- TaskPlayground.playground/Contents.swift
+++ TaskPlayground.playground/Contents.swift
@@ -1,13 +1,16 @@
 //: TaskPlayground: a simple helper for grouping together pieces of code that are run asynchronously
 
-//: The main requirement was to execute an async task on a background queue, followed by a UI update on the main queue, and be able to wait on the completion of both for the execution of yet another block of code
+//:
+// The main requirement was to execute an async task on a background queue, followed by a UI
+// update on the main queue, and be able to wait on the completion of both for the execution
+// of yet another block of code
 
 import Cocoa
-import XCPlayground
+import PlaygroundSupport
 
 public func defaultQueueProvider() -> TaskQueueProviderType {
     struct Provider : TaskQueueProviderType {
-        func getQueue() -> dispatch_queue_t {
+        func getQueue() -> DispatchQueue {
             return utilityQueue()
         }
     }
@@ -16,7 +19,7 @@ public func defaultQueueProvider() -> TaskQueueProviderType {
 
 public func backgroundQueueProvider() -> TaskQueueProviderType {
     struct Provider : TaskQueueProviderType {
-        func getQueue() -> dispatch_queue_t {
+        func getQueue() -> DispatchQueue {
             return backgroundQueue()
         }
     }
@@ -25,95 +28,95 @@ public func backgroundQueueProvider() -> TaskQueueProviderType {
 
 public func mainQueueProvider() -> TaskQueueProviderType {
     struct Provider : TaskQueueProviderType {
-        func getQueue() -> dispatch_queue_t {
+        func getQueue() -> DispatchQueue {
             return mainQueue()
         }
     }
     return Provider()
 }
 
-func mainQueue() -> dispatch_queue_t {
-    return dispatch_get_main_queue()
+func mainQueue() -> DispatchQueue {
+    return DispatchQueue.main
 }
 
-func userInitiatedQueue() -> dispatch_queue_t {
-    return dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)
+func userInitiatedQueue() -> DispatchQueue {
+    return DispatchQueue.global(qos: .userInitiated)
 }
 
-func utilityQueue() -> dispatch_queue_t {
-    return dispatch_get_global_queue(QOS_CLASS_UTILITY, 0)
+func utilityQueue() -> DispatchQueue {
+    return DispatchQueue.global(qos: .utility)
 }
 
-func backgroundQueue() -> dispatch_queue_t {
-    return dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)
+func backgroundQueue() -> DispatchQueue {
+    return DispatchQueue.global(qos: .background)
 }
 
 public protocol TaskQueueProviderType {
-    func getQueue() -> dispatch_queue_t
+    func getQueue() -> DispatchQueue
 }
 
 public typealias TaskBlock = () -> Void
 public struct TaskGroup {
     
-    let _group: dispatch_group_t
+    let _group: DispatchGroup
     let _queueProvider: TaskQueueProviderType
     
     public init() {
         self.init(defaultQueueProvider())
     }
     public init(_ queueProvider:TaskQueueProviderType) {
-        _group = dispatch_group_create()
+        _group = DispatchGroup()
         _queueProvider = queueProvider
     }
     
-    public func join(seconds timeout: Double! = nil, completion:TaskBlock) {
+    public func join(seconds timeout: Double! = nil, completion:@escaping TaskBlock) {
         if timeout != nil {
             join(millis: timeout*1000, completion: completion)
         } else {
-            dispatch_group_wait(_group, DISPATCH_TIME_FOREVER)
+            _group.wait(timeout: .distantFuture)
         }
     }
     
     public func join(millis timeout: Double! = nil, completion:TaskBlock?) {
         if let handler = completion {
-            dispatch_group_notify(_group, _queueProvider.getQueue()) {
+            _group.notify(queue: _queueProvider.getQueue()) {
                 handler()
             }
         }
         if timeout != nil {
-            let nanos = Int64(Double(timeout) * Double(NSEC_PER_MSEC))
-            let time = dispatch_time(DISPATCH_TIME_NOW, nanos)
-            dispatch_group_wait(_group, time)
+            let nanos = Int(Double(timeout) * Double(NSEC_PER_MSEC))
+            let time: DispatchTime = .now() + .nanoseconds(nanos)
+            _group.wait(timeout: time)
         } else {
-            dispatch_group_wait(_group, DISPATCH_TIME_FOREVER)
+            _group.wait(timeout: .distantFuture)
         }
     }
     
-    public func asyncMain(millis timeout: Double, block: TaskBlock) {
-        let nanos = Int64(Double(timeout) * Double(NSEC_PER_MSEC))
-        let time = dispatch_time(DISPATCH_TIME_NOW, nanos)
-        dispatch_after(time, dispatch_get_main_queue(), _exec(block))
+    public func asyncMainAfter(millis timeout: Double, block: @escaping TaskBlock) {
+        let nanos = Int(Double(timeout) * Double(NSEC_PER_MSEC))
+        let time: DispatchTime = .now() + .nanoseconds(nanos)
+        mainQueue().asyncAfter(deadline: time, execute: _exec(block))
     }
     
-    public func asyncMain(block: TaskBlock) {
-        dispatch_async(dispatch_get_main_queue(), _exec(block))
+    public func asyncMain(block: @escaping TaskBlock) {
+        mainQueue().async(execute: _exec(block))
     }
     
-    public func async(millis timeout: Double, block: TaskBlock) {
-        let nanos = Int64(Double(timeout) * Double(NSEC_PER_MSEC))
-        let time = dispatch_time(DISPATCH_TIME_NOW, nanos)
-        dispatch_after(time, _queueProvider.getQueue(), _exec(block))
+    public func asyncAfter(millis timeout: Double, block: @escaping TaskBlock) {
+        let nanos = Int(Double(timeout) * Double(NSEC_PER_MSEC))
+        let time: DispatchTime = .now() + .nanoseconds(nanos)
+        _queueProvider.getQueue().asyncAfter(deadline: time, execute: _exec(block))
     }
-    public func async(block: TaskBlock) {
-        dispatch_async(_queueProvider.getQueue(), _exec(block))
+    public func async(block: @escaping TaskBlock) {
+        _queueProvider.getQueue().async(execute: _exec(block))
     }
     
-    func _exec(block: TaskBlock) -> dispatch_block_t {
+    func _exec(_ block: @escaping TaskBlock) -> ()->Void {
         let group = self._group
-        dispatch_group_enter(group)
+        group.enter()
         return {
             block()
-            dispatch_group_leave(group)
+            group.leave()
         }
     }
     
@@ -121,30 +124,51 @@ public struct TaskGroup {
 
 // --------------------------------------
 
-dispatch_async(backgroundQueue()) {
-    print("Running async")
-    
+
+print("00-Starting background tasks")
+
+backgroundQueueProvider().getQueue().async {
+
+//    let timer = StopWatch()
+    print("10-Running async")
+
     let tg = TaskGroup(backgroundQueueProvider())
-    print("created")
-    
-    tg.async(millis: 3000) {
-        print("wait 3s - background queue")
+    print("20-group created")
+
+    tg.asyncAfter(millis: 3000) {
+        print("70-wait 3.0s - T1 background queue")
     }
-    print("Task1 created")
-    
-    tg.asyncMain(millis:1500) {
-        print("wait 1.5s (doFirst) - MAIN queue")
+    print("30-T1 started")
+
+    tg.asyncMainAfter(millis:1500) {
+        print("60-wait 1.5s (doFirst) - T2 MAIN queue")
     }
-    print("Task2 created")
-    
-    print("WAIT for completion")
+    print("40-T2 started")
+
+    print("50-WAIT for completion")
     tg.join(millis:20000) {
-        print("ALL COMPLETED")
+        print("110-ALL COMPLETED")
     }
-    print("WAITED")
-    
-    XCPlaygroundPage.currentPage.finishExecution()
+    print("120-WAITED")
+//    print ("Duration: \(timer.elapsed) \(timer.total)")
+
+    PlaygroundPage.current.finishExecution()
+
 }
 
+PlaygroundPage.current.needsIndefiniteExecution = true
 
-XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+/*:
+ Hello World!!!
+ 00-Starting background tasks
+ Duration: 0.007333993911743164
+ 10-Running async
+ 20-created
+ 30-Task1 created
+ 40-Task2 created
+ 100-WAIT for completion
+ 50-wait 1.5s (doFirst) - MAIN queue
+ 60-wait 3s - background queue
+ 110-ALL COMPLETED
+ 120-WAITED
+*/
